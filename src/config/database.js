581cc7fdd9e37e87064296f282dde67a07b90ed2
italo_jose <italo3720523@faculdadecci.com.br>
2025-07-17 18:46:14 -0300

    outro commit

--- src/config/database.js
+++ src/config/database.js
@@ -1,16 +1,15 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT, 10),
-    dialect: 'postgres',
-    logging: false,
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      rejectUnauthorized: false
+    }
   }
-);
+});
 
 module.exports = sequelize;
