6ca0649fe07649109a9aa9405ba429805589ddea
italo_jose <italo3720523@faculdadecci.com.br>
2025-07-17 17:30:30 -0300

    Post commit

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
+const bcrypt = require('bcrypt'); 
 const jwt = require('jsonwebtoken');
 
 router.post('/login', async (req, res) => {
